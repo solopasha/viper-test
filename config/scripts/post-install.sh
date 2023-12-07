6069b4674049bcafb49bbde2cac26aa2d79260e4
Heus-Sueh <98482469+Heus-Sueh@users.noreply.github.com>
2023-12-07 20:16:03 -0300

    teste 20 pro max

---
+++ config/scripts/post-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+
+# set sddm as default
+systemctl set-default graphical.target 
+
+set -oue pipefail
